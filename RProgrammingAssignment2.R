eaa435aa578896477dfffc239bce54cfc7202d5f
ezgigucuyener <37187052+ezgigucuyener@users.noreply.github.com>
2019-08-10 12:30:01 +0300

    Updated R Programming Week 3 Assignment

--- RProgrammingAssignment2.R
+++ RProgrammingAssignment2.R
@@ -1,17 +1,22 @@
 makeCacheMatrix <- function(arg2 = matrix()) {
+#initialize in as NULL, so that it will hold the value of matrix inverse
 in <- NULL 
 setting <- function(arg1) {
+  #define setting fuction to assign new
 arg2 <<- arg1
 in <<- NULL
 }
 getting <- function() arg2
 setinv <- function(inverse) in <<- inverse
-#in order to be able to access variables by using dollar sign.
+# assigns value of inv in parent environment
+
 getinv <- function() in
+#in order to be able to access variables by using dollar sign.
 list(setting = setting, setinv = setinv, getinv = getinv)
 }
 
 cacheSolve <- function(arg2, ...){
+#return a matrix that's inverse 
 in <- arg2$getinv()
 if(!is.null(in)){
 message("getting the cached data...")
